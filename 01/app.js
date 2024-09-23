ae0b2d6a006b15479c77620d36647f3d147315e7
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-09-23 18:25:51 +0200

    added solution to 1 task

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+const commentsItem = document.querySelector(".comments__item");
+const newWest = document.querySelector(".comments__item--newest");
+
+if (commentsItem && newWest) {
+    const dataInfo = document.querySelectorAll("[data-info]")
+    console.log(`Znaleziono ${dataInfo.length}`)
+};
