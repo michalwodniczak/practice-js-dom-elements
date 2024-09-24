a68e2809017045cc9faa92c6312b75e2cc77b0ea
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-09-24 16:31:57 +0200

    added solution to 4 task

--- 04/app.js
+++ 04/app.js
@@ -2,7 +2,53 @@ console.log('DOM');
 
 // struktura do wykorzystania w pętli
 const menuItems = [
-    {text: 'start', url: '/'},
-    {text: 'galeria', url: '/gallery'},
-    {text: 'kontakt', url: '/contact'},
+    { text: 'start', url: '/' },
+    { text: 'galeria', url: '/gallery' },
+    { text: 'kontakt', url: '/contact' },
 ];
+
+const nav = document.querySelector("nav");
+const ul = document.querySelector("ul");
+
+nav.innerHTML = `
+ <ul>
+   <li><a href="/">start</a></li>
+    <li><a href="/gallery">galeria</a></li>
+    <li><a href="/contact">kontakt</a></li>
+ </ul>
+`
+menuItems.forEach(link => {
+    const ul = document.querySelector("ul");
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.href = link.url;
+    a.textContent = link.text;
+    li.appendChild(a);
+    ul.appendChild(li);
+});
+
+nav.appendChild(ul);
+
+// Utwórz poniższą strukturę menu za pomocą JS:
+
+// ```
+// <ul>
+//     <li><a href="/">start</a></li>
+//     <li><a href="/gallery">galeria</a></li>
+//     <li><a href="/contact">kontakt</a></li>
+// </ul>
+// ```
+
+// Całość wstaw do elementu `<nav/>`.
+
+// Początkowo spróbuj wykonać to zadanie bez wykorzystania poniższej struktury:
+
+// ```
+// const menuItems = [
+//     {text: 'start', url: '/'},
+//     {text: 'galeria', url: '/gallery'},
+//     {text: 'kontakt', url: '/contact'},
+// ]
+// ```
+
+// Następnie zrób to samo, ale używając danych wraz z np. [forEach](https://developer.mozilla.org/pl/docs/Web/JavaScript/Referencje/Obiekty/Array/forEach).
